9049e18518a4c6a2f6b52a512436fc847ed66dad
uioyui <128584394+uioyui@users.noreply.github.com>
2024-11-16 09:53:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,6 +2,7 @@
 // @ts-ignore
 import { connect } from "cloudflare:sockets";
 
+
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = "77a571fb-4fd2-4b37-8596-1b7d9728bb5c";
